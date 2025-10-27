fe8aa1c195ecb00f69265bf7fccbddac2378ec0c
Calebe Lourenço Geronimo <131987253+CalebeLouGer@users.noreply.github.com>
2025-10-27 16:52:05 -0400

    Add files via upload

---
+++ screenmatch/src/Principal.java
@@ -0,0 +1,58 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroDeRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme favorito = new Filme();
+
+        favorito.setNome("Demon Slayer: Castelo Infinito");
+        favorito.setAnoDeLancamento(2025);
+        favorito.setDuracaoEmMinutos(185);
+        favorito.setIncluidoNoPlano(true);
+
+        favorito.exibirFichaTecnica();
+        favorito.avaliar(9);
+        favorito.avaliar(8.4);
+        favorito.avaliar(10);
+
+        String mensagemMedia = String.format("Média de avaliações do Filme: %.1f", favorito.retornarMedia());
+        //System.out.println(mensagemMedia);
+
+        Serie friends = new Serie();
+
+        friends.setNome("Friends");
+        friends.setAnoDeLancamento(1990);
+        friends.exibirFichaTecnica();
+        friends.setAtiva(true);
+        friends.setIncluidoNoPlano(true);
+        friends.setTemporada(10);
+        friends.setEpisodioPorTemporada(20);
+        friends.setMinutosPorEpisodio(23);
+        //System.out.println("Duração para maratonar Friends: "+friends.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calc = new CalculadoraDeTempo();
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Quarteto Fantástico: Origem");
+        outroFilme.setAnoDeLancamento(2025);
+        outroFilme.setDuracaoEmMinutos(200);
+        outroFilme.setIncluidoNoPlano(true);
+
+        calc.incluir(favorito);
+        calc.incluir(outroFilme);
+        calc.incluir(friends);
+        System.out.println("Tempo Total: "+calc.getTempoTotal());
+
+        FiltroDeRecomendacao filtro = new FiltroDeRecomendacao();
+        filtro.filtra(favorito);
+
+        Episodio ep = new Episodio();
+        ep.setNumero(1);
+        ep.setSerie(friends);
+        ep.setTotalVisualizacoes(90);
+        filtro.filtra(ep);
+    }
+}
---
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporada;
+    private boolean ativa;
+    private int episodioPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(int temporada) {
+        this.temporada = temporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    public int getEpisodioPorTemporada() {
+        return episodioPorTemporada;
+    }
+
+    public void setEpisodioPorTemporada(int episodioPorTemporada) {
+        this.episodioPorTemporada = episodioPorTemporada;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporada * episodioPorTemporada * minutosPorEpisodio;
+    }
+}
